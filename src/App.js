6b75650b1410e6e34ffcb12225562d49c2a90c0e
RaquelOliveira <ods.raquel@gmail.com>
2018-11-08 15:28:35 -0300

    layout twitter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
 	render () {
 		return (
 			<div className="App">
-				<h1>Twitter Clone</h1>
+				<header className="App-header"> 
+					<h1>Header</h1>
+				</header>
+				<div className="App-profile">
+				<h2>profile</h2>
+				</div>
+				<div className="App-card">
+				<h2>Card</h2>
+				</div>
+				<div className="App-follow">
+				<h2>follow</h2>
+				</div>
+				<div className="App-newCard">
+				<h2>new card</h2>
+				</div>
 			</div>
 		);
 	}
